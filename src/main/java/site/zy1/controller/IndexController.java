8f299c20f07e68ebf9397fe570c7a5590ae7dbee
Zeng Yilun <me10zyl@qq.com>
2016-08-24 15:49:33 +0800

    搭建 整体框架

--- src/main/java/site/zy1/controller/IndexController.java
+++ src/main/java/site/zy1/controller/IndexController.java
@@ -7,9 +7,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
-	@RequestMapping("/")
-	public String index(Model model,@Value("${app.version:}") String version) {
+	
+	@RequestMapping("/html_gen")
+	public String htmlGen(Model model,@Value("${app.version:}") String version) {
 		model.addAttribute("version", version);
+		return "html-gen";
+	}
+	
+	@RequestMapping("/orm_gen")
+	public String ormGen(Model model) {
+		return "orm-gen";
+	}
+	
+	@RequestMapping("/")
+	public String index(Model model){
 		return "index";
 	}
 }
